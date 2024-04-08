c7120e6edb1d6bef4105006c305e3835e199ced2
Blankxx420 <briceguichou.dev@gmail.com>
2024-04-08 23:40:16 +0200

    base

---
+++ philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brguicho <brguicho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 21:28:12 by brguicho          #+#    #+#             */
+/*   Updated: 2024/04/08 21:41:22 by brguicho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <string.h>
+
+int	philo(int argc, char **argv);
+
+#endif
